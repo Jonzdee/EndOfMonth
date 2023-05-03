72b4bc3020a7eb832e0167bafde07f5378f1a29f
jonzdee <ogunyankinjohnson@gmail.com>
2023-05-03 20:25:34 +0100

    EndOfMonth

---
+++ main.js
@@ -0,0 +1,18 @@
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyv8n1Cc2tEkH_PBGrqrRhUQN-CNySezcAJPbNuQXBcgM7tdf93rNA2FSvEPCTf5wyE/exec'
+const form = document.forms['submit-to-google-sheet']
+
+const msg = document.getElementById('msg')
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => { 
+      msg.innerHTML="Message sent successfully"
+    setTimeout(function(){
+      msg.innerHTML = ""
+    }, 500)
+    form.reset()
+  })
+    .catch(error => console.error('Error!', error.message))
+})
